90ee1c47c7baf9c07489312830992162d8ebbbb7
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-20 23:16:40 +0300

    addd build component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Copyright © 2023 motionScan. All rights reserved.</p>
+        </footer>)
+}
