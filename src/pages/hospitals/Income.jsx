fa92970dcca24626cde31d2ef5aa20c540d47a56
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-20 23:13:17 +0300

    added income page

---
+++ src/pages/hospitals/Income.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Income() {
+    return (
+        <div className="income--page">
+            <h1 className="center--text">This is income page</h1>
+        </div>
+    )
+}
