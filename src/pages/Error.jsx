7a8222e0c59a967117d45f9a4122366575126618
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-20 23:12:05 +0300

    added Error  page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Error()  {
+    return (
+        <div className="error--page">
+            <div className="error--page--container">
+                <h1>An errror occured</h1>
+            </div>
+        </div>
+    )
+}
