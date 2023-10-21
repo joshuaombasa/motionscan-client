3b3c64dbad47be9f8c9d33aad4fb86eab34e2620
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-21 20:34:08 +0300

    updated page content & logic

--- src/pages/hospitals/Income.jsx
+++ src/pages/hospitals/Income.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
 export default function Income() {
+    const incomeData = [
+        { amount: 720000, date: "February 15, 2023", id: "1" },
+        { amount: 560000, date: "March 8, 2023", id: "2" },
+        { amount: 980000, date: "April 20, 2023", id: "3" },
+    ];
+
+    const transactionElements = incomeData.map(item => (
+        <div className="transaction--item" key={item.id}>
+            <h3>Ksh {item.amount}</h3>
+            <h4>{item.date}</h4>
+        </div>
+    ))
     return (
         <div className="income--page">
-            <h1 className="center--text">This is income page</h1>
+            <div className="income--page--container">
+                <h1>Income</h1>
+                <p>Last 30 days</p>
+                <span>Ksh 3000000</span>
+                <div className="transactions">
+                    <h2>Your transactions</h2>
+                    <p>Last 30 days</p>
+                </div>
+                {transactionElements}
+            </div>
         </div>
     )
 }
