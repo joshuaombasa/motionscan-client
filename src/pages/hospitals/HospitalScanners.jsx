ba561a593a9a80b9b8842df4770e6b0646a9e7f0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-24 21:29:03 +0300

    Added Suspense component renders a loading message when lazy loading

--- src/pages/hospitals/HospitalScanners.jsx
+++ src/pages/hospitals/HospitalScanners.jsx
@@ -1,26 +1,41 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Await, defer, useLoaderData } from "react-router-dom";
 import { getHospitalScanners } from "../../api";
 import SingleHospitalScanner from "../../components/SingleHospitalScanner";
 
 export async function loader() {
-    return getHospitalScanners()
+    return defer({ hospitalScanners: getHospitalScanners() })
 }
 
 export default function HospitalScanners() {
-    const data = useLoaderData()
-   
-    const hospitalScannerElements = data.map(scanner => (
-        <SingleHospitalScanner key={scanner.id} scanner={scanner}/>
-    ))
+    const dataPromise = useLoaderData()
 
-    
+
+
+    function renderHospitalScanners(data) {
+
+        const hospitalScannerElements = data.map(scanner => (
+            <SingleHospitalScanner key={scanner.id} scanner={scanner} />
+        ))
+
+        return (
+            <>
+                <h1>Your listed  scanners</h1>
+                <div className="hospital--scanners--container">
+                    {hospitalScannerElements}
+                </div>
+            </>
+        )
+    }
+
+
     return (
         <div className="hospital--scanners--page">
-            <h1>Your listed  scanners</h1>
-            <div className="hospital--scanners--container">
-                {hospitalScannerElements}
-            </div>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.hospitalScanners}>
+                    {renderHospitalScanners}
+                </Await>
+            </Suspense>
         </div>
     )
 }
