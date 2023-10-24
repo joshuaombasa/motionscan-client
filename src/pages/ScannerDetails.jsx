45e2b800e103cbe1c2b1bc346d519132d97cc37f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-24 21:28:48 +0300

    Added Suspense component renders a loading message when lazy loading

--- src/pages/ScannerDetails.jsx
+++ src/pages/ScannerDetails.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Await, defer, Link, useLoaderData, useLocation } from "react-router-dom";
 import { getHospitalDetails } from "../api";
 
-export async function loader({params}) {
-    return defer({hospitalDetails: getHospitalDetails(params.id)})
+export async function loader({ params }) {
+    return defer({ hospitalDetails: getHospitalDetails(params.id) })
 }
 
 export default function ScannerDetails() {
@@ -12,38 +12,41 @@ export default function ScannerDetails() {
 
     const dataPromise = useLoaderData()
 
-    const statusStyle = {
-        backgroundColor: hospital.status === "working" ? "green" : "red"
-    }
-
-    const params = location.state.searchParameters ? location.state.searchParameters :""
+    const params = location.state.searchParameters ? location.state.searchParameters : ""
     const filterType = location.state.filterType ? location.state.filterType : "all"
 
     function renderHospitalDetails(hospital) {
+
+        const statusStyle = {
+            backgroundColor: hospital.status === "working" ? "green" : "red"
+        }
+        
         return (
             <>
-              <Link 
-               to={`..?${params}`} 
-               relative="path" 
-               className="back--link"
-            >&larr; Back to {filterType} scanners</Link>
-            <div className="hospital--details--page--container">
-                <img src={hospital.imageUrl} alt="" />
-                <span style={statusStyle}>{hospital.status}</span>
-                <h2>{hospital.name}</h2>
-                <h3>Ksh {hospital.price}</h3>
-                <p>{hospital.description}</p>
-                <Link to="/">Book a session</Link>
-            </div>
+                <Link
+                    to={`..?${params}`}
+                    relative="path"
+                    className="back--link"
+                >&larr; Back to {filterType} scanners</Link>
+                <div className="hospital--details--page--container">
+                    <img src={hospital.imageUrl} alt="" />
+                    <span style={statusStyle}>{hospital.status}</span>
+                    <h2>{hospital.name}</h2>
+                    <h3>Ksh {hospital.price}</h3>
+                    <p>{hospital.description}</p>
+                    <Link to="/">Book a session</Link>
+                </div>
             </>
         )
     }
 
     return (
         <div className="hospital--details--page">
-            <Await resolve={dataPromise.hospitalDetails}>
-                {renderHospitalDetails}
-            </Await>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.hospitalDetails}>
+                    {renderHospitalDetails}
+                </Await>
+            </Suspense>
         </div>
     )
 }
