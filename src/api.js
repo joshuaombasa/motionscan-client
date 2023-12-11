8d39bc3842d12507de01598a02345bd6dabd6290
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2023-12-11 06:05:40 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,17 @@
 
 
 export async function getAllHospitalMachines() {
-    
+    const res = await fetch("http://localhost:3000/api/scanners")
+    const data = await res.json()
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            status: res.status,
+            statusText: res.statusText,
+        }
+    }
+
+    return data
 }
 
 export async function getHospitalDetails(id) {
